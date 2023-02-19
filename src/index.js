b61cf4fbacdf864b05a4c875f36c392f60f0bd74
Boris Shagiev <boris260692@gmail.com>
2023-02-19 23:33:00 +0200

    Fix router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='courses-cursor-react-20'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
